63ef6e1a50fdb2b94cc885748abaf05643d6c50b
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-06 10:25:44 +0600

    add Home Work_02

---
+++ HomeWork_02/Program.cs
@@ -0,0 +1,24 @@
+﻿int a, b, c;
+ 
+Console.WriteLine("Введите 3 числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+ 
+if ((a > b) && (a > c))
+ 
+{
+    Console.WriteLine(a);
+}
+ 
+if ((b > a) && (b > c))
+
+{
+    Console.WriteLine(b);
+}
+
+if ((c > a) && (c > b))
+        
+{
+    Console.WriteLine(c);
+}
