3da1ca525badc475ed1314fa2768bca4271ebcfe
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-06 10:30:49 +0600

    add Home Work_03

---
+++ HomeWork_03/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+Console.WriteLine("Введите число и проверим его на четность");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
